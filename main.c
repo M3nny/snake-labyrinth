e3c56f147b305481837612c375ff8addd4f60faa
M3nny <77993658+M3nny@users.noreply.github.com>
2022-10-26 22:38:01 +0200

    Create a function that prints lab. size

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
-int main() {
-
+void get_labyrinth_size (const char *stage) {
     FILE* file = fopen("labyrinth.txt", "r");
 
     if(!file) {
         printf("\n Unable to open file");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     char line[50];
@@ -16,7 +16,7 @@ int main() {
     bool selected = false;
 
     while (fgets(line, sizeof(line), file)) { //leggo tutto il file e stoppo il ciclo dopo aver letto il labirinto
-        if ((strcmp(line, "1\n")) == 0) { // se trovo il numero corrispondente al labirinto comincio a leggerlo
+        if ((strcmp(line, stage)) == 0) { // se trovo il numero corrispondente al labirinto comincio a leggerlo
             selected = true;
             while (fgets(line, sizeof(line), file)) { // leggo il labirinto finchè non trovo end
                 if ((strcmp(line, "end\n")) == 0) {
@@ -32,5 +32,12 @@ int main() {
     }
     fclose(file);
     printf("righe: %d\ncolonne: %d\n", rows, columns);
+
+}
+int main() {
+    char stage[2] = "x\n";
+    scanf("%c", &stage[0]);
+    get_labyrinth_size(stage);
+
     return 0;
 }
